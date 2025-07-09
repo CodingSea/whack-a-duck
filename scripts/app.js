84183630a6163b13786e37ceb0f3df50712172c1
CodingSea <Fhdq2003@email.com>
2025-07-09 13:33:05 +0300

    added duck functionality and score

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,42 @@
 function init()
 {
-    // all coe goes here
     const gridElem = document.querySelector('.grid');
 
     const cells = [];
     const gridWidth = 10;
     const numberOfCells = gridWidth * gridWidth;
 
+    const scoreElem = document.querySelector("#score");
+    let score = 0;
+
+    let duckPosition = 20;
+
+    function addDuck()
+    {
+        cells[duckPosition].classList.add("duck");
+    }
+
+    function removeDuck()
+    {
+        cells[duckPosition].classList.remove("duck");
+    }
+
+    function play()
+    {
+        setInterval(() =>
+        {
+            removeDuck();
+            duckPosition = Math.floor(Math.random() * numberOfCells);
+            addDuck();
+        }, 3000);
+    }
+
     function createGrid()
     {
         for(let i = 0; i < numberOfCells; i++)
         {
             const cell = document.createElement("div");
-            cell.classList.add("duck");
+            //cell.classList.add("duck");
             //cell.textContent = i;
             cells.push(cell);
             gridElem.appendChild(cell);
@@ -22,6 +46,20 @@ function init()
     }
 
     createGrid();
+    addDuck();
+    //play();
+
+    gridElem.addEventListener('click', function(event)
+    {
+        if(event.target.classList.contains("duck"))
+        {
+            removeDuck();
+            duckPosition = Math.floor(Math.random() * numberOfCells);
+            addDuck();
+            score += 100;
+            scoreElem.textContent = score;
+        }
+    })
 }
 
 
