961caf20f468cb488116bf39ee31d4bdc379c737
CodingSea <Fhdq2003@email.com>
2025-07-09 11:14:43 +0300

    created a grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,19 @@
 function init()
 {
+    // all coe goes here
+    const gridElem = document.querySelector('.grid');
 
+    function createGrid()
+    {
+        for(let i = 0; i < 100; i++)
+        {
+            const cell = document.createElement("div");
+            cell.textContent = i;
+            gridElem.appendChild(cell);
+        }
+    }
+
+    createGrid();
 }
 
 
